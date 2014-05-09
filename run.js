0dd93e8455c26a4729efadaf6d1ad9584029eb30
Andrew Friedrich <afriedrich@glgroup.com>
2014-05-09 11:46:57 -0500

    done

---
+++ run.js
@@ -0,0 +1,44 @@
+var exec = require('child_process').exec;
+var fs = require('fs');
+
+var parts_file = process.env.HOME + '/parts.json';
+var declined_file = process.env.HOME + '/declined.json';
+var committed_file = process.env.HOME + '/committed.json';
+
+exec('./get_uncommitted', function(error, stdout, stderr) {
+
+	if (stdout.indexOf('<html...')){//unauthed
+		exec('./create_auth_cookie');
+		return;
+	}
+
+	var data = JSON.parse(stdout);
+	var parts_with_stop_sale = {};
+
+	data.d.Committed.Parts.forEach ( function (part) {
+		if (part.status == "Cancelled") {
+			parts_with_stop_sale[part.PartNo] = true;
+		}
+	});
+
+	if (data.d.Uncommitted.Parts.length > 0){
+		//Log there is an uncommitted part available
+		//var message = { time: new Date(), parts: data.d.Uncommitted.Parts, data: data };
+		//fs.appendFile(parts_file, JSON.stringify(message));
+
+		data.d.Uncommitted.Parts.forEach( function (part){
+			
+			if (parts_with_stop_sale[part.PartNo]){//The part has had a stop sale today, tell it not to remind us
+				exec('./decline_sale ' + part.ELLocID + ' ' + part.ELTransID, function(error, stdout, stderr) {
+					var message = { time: new Date(), part: part };
+					fs.appendFile(declined_file, JSON.stringify(message));
+				});
+			} else {//Attempt to commit sale of part
+				exec('./commit_sale ' + part.ELLocID + ' ' + part.ELTransID, function(error, stdout, stderr) {
+					var message = { time: new Date(), part: part };
+					fs.appendFile(committed_file, JSON.stringify(message));
+				});
+			}
+		});
+	}
+});
